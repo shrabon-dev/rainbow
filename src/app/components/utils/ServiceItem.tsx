3e29746944933f5f7e6288d0ec39c6aaf2697895
Md. Shrabon Hossain <90843040+shrabon-dev@users.noreply.github.com>
2025-07-05 15:22:26 +0600

    responsive done with 60%

--- src/app/components/utils/ServiceItem.tsx
+++ src/app/components/utils/ServiceItem.tsx
@@ -10,14 +10,14 @@ export default function ServiceItem({title,desc,link,img,id,}:{title?:string,des
         setCurrentSlide(currentSlide)
     },[])
   return (
-    <div className={`max-w-[960px] mx-auto shadow-2xl rounded-xl bg-white p-10 ${currentSlide == id ? 'relative z-50':`absolute left-1/2 -translate-x-1/2`} ${id == 1 && 'top-[150px] z-50'} ${id == 2 && 'top-[70px] scale-90 z-40  blur-[2px]'} ${id == 3 && 'top-[-0px] scale-80 z-30  blur-[2px]'} ${id == 4 && 'top-[-70px] z-20 scale-70 blur-[2px]'}`}>
-        <div className="lg:flex ">
-            <div className="w-full lg:w-1/3 img">
-                <div className="img rounded-lg overflow-hidden aspect-square">
-                    {img && <Image src={img} alt="serviceImage"/> }
+    <div className={`w-[300px] sm:w-[560px] md:w-[760px] lg:w-[960px] mx-auto shadow-2xl rounded-xl bg-white p-10 ${currentSlide == id ? 'relative z-50':`absolute left-1/2 -translate-x-1/2`} ${id == 1 && 'top-[150px] z-50'} ${id == 2 && 'top-[70px] scale-90 z-40  blur-[2px]'} ${id == 3 && 'top-[-0px] scale-80 z-30  blur-[2px]'} ${id == 4 && 'top-[-70px] z-20 scale-70 blur-[2px]'}`}>
+        <div className="md:flex ">
+            <div className="w-full md:w-1/3 img">
+                <div className="img rounded-lg overflow-hidden w-full lg:aspect-square">
+                    {img && <Image className="w-full" src={img} alt="serviceImage"/> }
                 </div>
             </div>
-            <div className="w-full lg:w-2/3 txt lg:pl-12 pt-5 lg:pt-0">
+            <div className="w-full md:w-2/3 txt md:pl-6 lg:pl-12 pt-5 md:pt-0">
                 <span className="rounded-full px-5 py-1 inline-block font-bold bg-clr-secondary"><GradientColor text={`Service ${id}`}/></span> 
                 <h3  className=" text-clr text-xl xl:text-5xl font-semibold my-3">{title}</h3>
                 <p className=" text-clr-text text-sm lg:text-lg font-normal leading-8 pb-5">{desc}</p>
